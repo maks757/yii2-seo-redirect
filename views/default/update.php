6d64bf8fda56bb25e9264de24df44581fb1a5f89
Cherednyk Maxim <maks757q@gmail.com>
2018-03-20 15:34:37 +0200

    Update update.php

--- views/default/update.php
+++ views/default/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model maks757\seoredirect\entities\RedirectTools */
 
-$this->title = Yii::t('app', 'Update Redirect Tools: {nameAttribute}', [
+$this->title = Yii::t('app', 'Update Redirect: {nameAttribute}', [
     'nameAttribute' => $model->id,
 ]);
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Redirect Tools'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Redirect'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
