560a609f2ea3d83ef2a897cc117bbc7fae08925a
Cherednyk Maxim <maks757q@gmail.com>
2018-03-20 15:14:54 +0200

    Update SeoRedirectComponent.php

--- components/SeoRedirectComponent.php
+++ components/SeoRedirectComponent.php
@@ -10,10 +10,24 @@ namespace maks757\seoredirect\components;
 
 
 use maks757\seoredirect\entities\RedirectTools;
+use yii\base\Behavior;
+use yii\web\Controller;
 
-class SeoRedirectComponent
+class SeoRedirectComponent extends Behavior
 {
-    public static function chackUrl(){
+    public function events()
+    {
+        return [
+            Controller::EVENT_BEFORE_ACTION => 'beforeAction',
+        ];
+    }
+
+    public function beforeAction($event)
+    {
+        $this->chackUrl();
+    }
+
+    protected static function chackUrl(){
         $redirect = RedirectTools::findOne(['from' => \Yii::$app->getRequest()->getAbsoluteUrl()]);
         if(!empty($redirect)) {
             \Yii::$app->getResponse()->redirect($redirect->to, $redirect->code)->send();
